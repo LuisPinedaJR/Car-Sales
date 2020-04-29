b7ca1805c3122cb051a74837d29a0f85c9273b71
Luis Pineda <lspnd00@gmail.com>
2020-04-28 21:09:10 -0400

    finished mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import rootReducer from './reducers'
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import App from './App'
+
+import './styles.scss'
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+// console.log("store from src/index.js", store);
+
+const rootElement = document.getElementById('root')
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
