a44354ef705dc503fa9f74016eb63a26796a922e
Luis Pineda <lspnd00@gmail.com>
2020-04-28 22:04:14 -0400

    added dev tools and change type code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 
 import App from './App'
 
 import './styles.scss'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+)
 // console.log("store from src/index.js", store);
 
 const rootElement = document.getElementById('root')
